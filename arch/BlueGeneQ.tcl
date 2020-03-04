b3cc3b5994f8fc61dbec1d5a14fe27d1bac8b414
Jim Phillips <jim@ks.uiuc.edu>
2020-03-04 09:48:08 -0600

    Remove obsolete build targets (including all 32-bit)
    Change-Id: If23f666f7807f8654e521c5ba7c09b1b75f803d0

--- arch/BlueGeneQ.tcl
+++
@@ -1,7 +0,0 @@
-
-TCLDIR=$(HOME)/tcl8.5.9-bluegeneq-threaded
-TCLINCL=-I$(TCLDIR)/include
-TCLLIB=-L$(TCLDIR)/lib -ltcl8.5
-TCLFLAGS=-DNAMD_TCL
-TCL=$(TCLINCL) $(TCLFLAGS)
-
--- arch/Linux-ARM.tcl
+++
@@ -1,9 +0,0 @@
-
-#TCLDIR=/Projects/namd2/tcl/tcl8.5.9-linux-arm
-TCLDIR=/Projects/namd2/tcl/tcl8.5.9-linux-arm-threaded
-TCLINCL=-I$(TCLDIR)/include
-#TCLLIB=-L$(TCLDIR)/lib -ltcl8.5 -ldl
-TCLLIB=-L$(TCLDIR)/lib -ltcl8.5 -ldl -lpthread
-TCLFLAGS=-DNAMD_TCL
-TCL=$(TCLINCL) $(TCLFLAGS)
-
