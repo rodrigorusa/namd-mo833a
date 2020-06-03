d21baf7ef00d1d2bf3bab8098498fdee77ed6639
Rodrigo Rusa <rodrigorusa@gmail.com>
2020-06-03 09:15:24 -0300

    Fix path on config files

--- arch/Linux-x86_64.tcl
+++ arch/Linux-x86_64.tcl
@@ -1,6 +1,6 @@
 
 #TCLDIR=/Projects/namd2/tcl/tcl8.5.9-linux-x86_64
-TCLDIR=/Projects/namd2/tcl/tcl8.5.9-linux-x86_64-threaded
+TCLDIR=/Projects/namd2/tcl-threaded
 TCLINCL=-I$(TCLDIR)/include
 #TCLLIB=-L$(TCLDIR)/lib -ltcl8.5 -ldl
 TCLLIB=-L$(TCLDIR)/lib -ltcl8.5 -ldl -lpthread
